2390e843ab3d073fe952c8bd60358242374a463f
AmarHasecic <ahasecic1@etf.unsa.ba>
2023-06-24 20:35:46 +0200

    Camera positioned to display devices location on the map.

--- app/src/main/java/ba/unsa/sportevents/main_page/tabs/MapScreen.kt
+++ app/src/main/java/ba/unsa/sportevents/main_page/tabs/MapScreen.kt
@@ -1,54 +1,55 @@
 package ba.unsa.sportevents.main_page.tabs
 
-import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
-import android.location.Location
+
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Switch
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 
-private const val REQUEST_LOCATION_PERMISSION = 123
-
 
 @Composable
 fun MapScreen(token: String){
     var uiSettings by remember { mutableStateOf(MapUiSettings()) }
-    var properties by remember {
+    val properties by remember {
         mutableStateOf(MapProperties(mapType = MapType.NORMAL))
     }
 
-    val context = LocalContext.current
 
-    val sarajevo = LatLng(43.857988, 18.413247)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(sarajevo, 10f)
+
+    val context: Context = LocalContext.current
+    val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    var longLat by remember { mutableStateOf(LatLng(0.0, 0.0)) }
+    var cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(longLat, 10f)
     }
 
 
-    LaunchedEffect(Unit){
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                context as Activity,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION_PERMISSION
-            )
-        }
+    LaunchedEffect(Unit) {
+
+        //dodati provjeru je li dozvoljena lokacija
+            @SuppressLint("MissingPermission")
+            val location = fusedLocationClient.lastLocation
+            location.addOnCompleteListener { task ->
+                if (task.isSuccessful && task.result != null) {
+                    val latitude = task.result.latitude
+                    val longitude = task.result.longitude
+                    longLat = LatLng(latitude, longitude)
+                    cameraPositionState.position = CameraPosition.fromLatLngZoom(longLat, 10f)
+                }
+            }
+
+
+            //fetch all activities around me
+
     }
 
     Box(Modifier.fillMaxSize()) {
@@ -65,5 +66,4 @@ fun MapScreen(token: String){
             }
         )
     }
-
 }
